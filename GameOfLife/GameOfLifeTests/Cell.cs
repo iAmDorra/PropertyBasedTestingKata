1bc1445ac1b3de0526952cbcbf1ee9599522926e
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2022-08-20 12:00:05 +0200

    refactor classes

---
+++ GameOfLife/GameOfLifeTests/Cell.cs
@@ -0,0 +1,62 @@
+﻿using NFluent;
+
+namespace GameOfLifeTests
+{
+    public class Cell
+    {
+        private bool isAlive;
+
+        public Cell(bool isAlive = false)
+        {
+            this.isAlive = isAlive;
+        }
+
+        internal static Cell Alive()
+        {
+            return new Cell(true);
+        }
+
+        internal static Cell Dead()
+        {
+            return new Cell(false);
+        }
+
+        internal bool IsAlive()
+        {
+            return isAlive;
+        }
+
+        internal Cell NextGeneration(List<Cell> neighbors)
+        {
+            if (WithinStabilityThreshold(neighbors))
+            {
+                return this;
+            }
+
+            if (IsFertilityThreshold(neighbors))
+            {
+                return Cell.Alive();
+            }
+            return Cell.Dead();
+        }
+
+        private static bool IsFertilityThreshold(List<Cell> neighbors)
+        {
+            return neighbors.Count(neighbor => neighbor.isAlive) == 3;
+        }
+
+        private static bool WithinStabilityThreshold(List<Cell> neighbors)
+        {
+            return neighbors.Count(neighbor => neighbor.isAlive) == 2;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            var cell = obj as Cell;
+            if (cell == null)
+                return base.Equals(obj);
+            else
+                return cell.isAlive == this.isAlive;
+        }
+    }
+}
--- GameOfLife/GameOfLifeTests/CellTest.cs
+++ GameOfLife/GameOfLifeTests/CellTest.cs
@@ -183,71 +183,12 @@ namespace GameOfLifeTests
                 .NextGeneration(neighbors)
                 .IsAlive());
         }
-
-        public static class DeadCellGenerator
-        {
-            public static Arbitrary<List<Cell>> Generate()
-            {
-                return Arb.Default.List<Cell>().Filter(l => l.All(c => !c.IsAlive()));
-            }
-        }
     }
-
-    public class Cell
+    public static class DeadCellGenerator
     {
-        private bool isAlive;
-
-        public Cell(bool isAlive = false)
+        public static Arbitrary<List<Cell>> Generate()
         {
-            this.isAlive = isAlive;
-        }
-
-        internal static Cell Alive()
-        {
-            return new Cell(true);
-        }
-
-        internal static Cell Dead()
-        {
-            return new Cell(false);
-        }
-
-        internal bool IsAlive()
-        {
-            return isAlive;
-        }
-
-        internal Cell NextGeneration(List<Cell> neighbors)
-        {
-            if (WithinStabilityThreshold(neighbors))
-            {
-                return this;
-            }
-
-            if (IsFertilityThreshold(neighbors))
-            {
-                return Cell.Alive();
-            }
-            return Cell.Dead();
-        }
-
-        private static bool IsFertilityThreshold(List<Cell> neighbors)
-        {
-            return neighbors.Count(neighbor => neighbor.isAlive) == 3;
-        }
-
-        private static bool WithinStabilityThreshold(List<Cell> neighbors)
-        {
-            return neighbors.Count(neighbor => neighbor.isAlive) == 2;
-        }
-
-        public override bool Equals(object? obj)
-        {
-            var cell = obj as Cell;
-            if (cell == null)
-                return base.Equals(obj);
-            else
-                return cell.isAlive == this.isAlive;
+            return Arb.Default.List<Cell>().Filter(l => l.All(c => !c.IsAlive()));
         }
     }
 }
