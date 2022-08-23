d484d09948712a533e356d7eefc31c9a2555f1a4
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2022-08-23 23:19:24 +0200

    simple examples of sum associativity

--- GameOfLife/GameOfLifeTests/SumTest.cs
+++ GameOfLife/GameOfLifeTests/SumTest.cs
@@ -1,4 +1,6 @@
-﻿using NFluent;
+﻿using FsCheck;
+using FsCheck.Xunit;
+using NFluent;
 
 namespace GameOfLifeTests
 {
@@ -12,6 +14,47 @@ namespace GameOfLifeTests
             int c = 3;
             NFluent.Check.That((a + b) + c)
                 .IsEqualTo(a + (b + c));
+        }
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(10, 32, -73)]
+        public void Sum_associativity(int a, int b, int c)
+        {
+            NFluent.Check.That((a + b) + c)
+                .IsEqualTo(a + (b + c));
+        }
+
+        [Property]
+        public Property Associativity_of_sum(int a, int b, int c)
+        {
+            return Prop.When(Global.AnyInput,
+                () => (a + b) + c == a + (b + c));
+        }
+
+        [Property]
+        public Property Associativity_of_sum_with_positif_numbers(int a, int b, int c)
+        {
+            return Prop.When(a > 0 && b > 0 && c > 0,
+                () => (a + b) + c == a + (b + c));
+        }
+
+        [Property(Arbitrary = new[] { typeof(PositiveNumberGenerator) })]
+        public Property AssociativityWith_positive_numbers(int a, int b, int c)
+        {
+            return Prop.When(Global.AnyInput,
+                () => (a + b) + c == a + (b + c));
+        }
+
+        public static class PositiveNumberGenerator
+        {
+            public static Arbitrary<int> Generate()
+            {
+                return Arb.Default.Int32()
+                    .MapFilter(
+                    x => Math.Abs(x),
+                    x => Global.AnyInput);
+            }
         }
     }
 }
