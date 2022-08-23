4b015d56f32ffeaddc90c13841d02a42023fef59
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2022-08-23 23:12:40 +0200

    Reorganize tests using the order of rules in the read me

---
+++ GameOfLife/GameOfLifeTests/Cell.cs
@@ -0,0 +1,72 @@
+﻿using NFluent;
+
+namespace GameOfLifeTests
+{
+    public class Cell
+    {
+        private bool isAlive;
+
+        public Cell(bool isAlive = false)
+        {
+            this.isAlive = isAlive;
+        }
+
+        internal static Cell Alive()
+        {
+            return new Cell(true);
+        }
+
+        internal static Cell Dead()
+        {
+            return new Cell(false);
+        }
+
+        internal bool IsAlive()
+        {
+            return isAlive;
+        }
+
+        internal bool IsDead()
+        {
+            return !IsAlive();
+        }
+
+        internal Cell NextGeneration(IEnumerable<Cell> neighbors)
+        {
+            if (WithinStabilityThreshold(neighbors))
+            {
+                return this;
+            }
+
+            if (IsFertilityThreshold(neighbors))
+            {
+                return Cell.Alive();
+            }
+            return Cell.Dead();
+        }
+
+        private static bool IsFertilityThreshold(IEnumerable<Cell> neighbors)
+        {
+            return neighbors.Count(neighbor => neighbor.isAlive) == 3;
+        }
+
+        private static bool WithinStabilityThreshold(IEnumerable<Cell> neighbors)
+        {
+            return neighbors.Count(neighbor => neighbor.isAlive) == 2;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            var cell = obj as Cell;
+            if (cell == null)
+                return base.Equals(obj);
+            else
+                return cell.isAlive == this.isAlive;
+        }
+
+        public override string ToString()
+        {
+            return isAlive ? "X" : " ";
+        }
+    }
+}
--- GameOfLife/GameOfLifeTests/CellTest.cs
+++ GameOfLife/GameOfLifeTests/CellTest.cs
@@ -18,15 +18,8 @@ namespace GameOfLifeTests
             NFluent.Check.That(aliveCell.IsAlive()).IsTrue();
         }
 
-        [Fact]
-        public void Dead_cell_with_no_neighbor_still_dead()
-        {
-            Cell deadCell = Cell.Dead();
-            var neighbors = new List<Cell>();
-            var cell = deadCell.NextGeneration(neighbors);
-            NFluent.Check.That(cell).IsEqualTo(Cell.Dead());
-        }
-
+        #region For a space that is populated
+        #region Each cell with one or no neighbors dies, as if by solitude
         [Fact]
         public void Alive_cell_with_no_neighbor_dies()
         {
@@ -37,14 +30,58 @@ namespace GameOfLifeTests
         }
 
         [Fact]
-        public void Dead_cell_with_3_neighbor_survives()
+        public void Alive_cell_with_1_neighbor_dies()
         {
-            Cell deadCell = Cell.Dead();
+            Cell deadCell = Cell.Alive();
+            var neighbors = new List<Cell>
+            {
+                Cell.Alive(),
+            };
+            var cell = deadCell.NextGeneration(neighbors);
+            Check.That(cell).IsEqualTo(Cell.Dead());
+        }
+        #endregion
+
+        #region Each cell with four or more neighbors dies, as if by overpopulation
+        [Fact]
+        public void Alive_cell_with_4_neighbor_still_dead()
+        {
+            Cell deadCell = Cell.Alive();
             var neighbors = new List<Cell>
             {
                 Cell.Alive(),
                 Cell.Alive(),
                 Cell.Alive(),
+                Cell.Alive(),
+            };
+            var cell = deadCell.NextGeneration(neighbors);
+            Check.That(cell).IsEqualTo(Cell.Dead());
+        }
+        #endregion
+
+        #region Each cell with two or three neighbors survives. For a space that is empty or unpopulated
+        [Fact]
+        public void Alive_cell_with_2_neighbors_still_alive()
+        {
+            Cell deadCell = Cell.Alive();
+            var neighbors = new List<Cell>
+            {
+                Cell.Alive(),
+                Cell.Alive(),
+            };
+            var cell = deadCell.NextGeneration(neighbors);
+            Check.That(cell).IsEqualTo(Cell.Alive());
+        }
+
+        [Fact]
+        public void Alive_cell_with_2_alive_neighbors_survives()
+        {
+            Cell deadCell = Cell.Alive();
+            var neighbors = new List<Cell>
+            {
+                Cell.Alive(),
+                Cell.Alive(),
+                Cell.Dead(),
             };
             var cell = deadCell.NextGeneration(neighbors);
             Check.That(cell).IsEqualTo(Cell.Alive());
@@ -63,85 +100,23 @@ namespace GameOfLifeTests
             var cell = deadCell.NextGeneration(neighbors);
             Check.That(cell).IsEqualTo(Cell.Alive());
         }
+        #endregion
+        #endregion
 
+        #region For a space that is empty or unpopulated
+        #region Each cell with three neighbors becomes populated
         [Fact]
-        public void Dead_cell_with_4_neighbor_still_dead()
+        public void Dead_cell_with_3_neighbor_survives()
         {
             Cell deadCell = Cell.Dead();
             var neighbors = new List<Cell>
             {
                 Cell.Alive(),
-                Cell.Alive(),
-                Cell.Alive(),
-                Cell.Alive(),
-            };
-            var cell = deadCell.NextGeneration(neighbors);
-            Check.That(cell).IsEqualTo(Cell.Dead());
-        }
-
-        [Fact]
-        public void Alive_cell_with_4_neighbor_still_dead()
-        {
-            Cell deadCell = Cell.Alive();
-            var neighbors = new List<Cell>
-            {
-                Cell.Alive(),
-                Cell.Alive(),
-                Cell.Alive(),
-                Cell.Alive(),
-            };
-            var cell = deadCell.NextGeneration(neighbors);
-            Check.That(cell).IsEqualTo(Cell.Dead());
-        }
-
-        [Fact]
-        public void Alive_cell_with_2_neighbors_still_alive()
-        {
-            Cell deadCell = Cell.Alive();
-            var neighbors = new List<Cell>
-            {
                 Cell.Alive(),
                 Cell.Alive(),
             };
             var cell = deadCell.NextGeneration(neighbors);
             Check.That(cell).IsEqualTo(Cell.Alive());
-        }
-
-        [Fact]
-        public void Dead_cell_with_2_neighbors_still_dead()
-        {
-            Cell deadCell = Cell.Dead();
-            var neighbors = new List<Cell>
-            {
-                Cell.Alive(),
-                Cell.Alive(),
-            };
-            var cell = deadCell.NextGeneration(neighbors);
-            Check.That(cell).IsEqualTo(Cell.Dead());
-        }
-
-        [Fact]
-        public void Alive_cell_with_1_neighbor_dies()
-        {
-            Cell deadCell = Cell.Alive();
-            var neighbors = new List<Cell>
-            {
-                Cell.Alive(),
-            };
-            var cell = deadCell.NextGeneration(neighbors);
-            Check.That(cell).IsEqualTo(Cell.Dead());
-        }
-
-        [Fact]
-        public void Dead_cell_with_1_neighbor_dies()
-        {
-            Cell deadCell = Cell.Dead();
-            var neighbors = new List<Cell>
-            {
-                Cell.Alive(),
-            };
-            var cell = deadCell.NextGeneration(neighbors);
-            Check.That(cell).IsEqualTo(Cell.Dead());
         }
 
         [Fact]
@@ -158,77 +133,56 @@ namespace GameOfLifeTests
             var cell = deadCell.NextGeneration(neighbors);
             Check.That(cell).IsEqualTo(Cell.Alive());
         }
+        #endregion
 
         [Fact]
-        public void Dead_cell_with_2_alive_neighbors_survives()
+        public void Dead_cell_with_no_neighbor_still_dead()
         {
-            Cell deadCell = Cell.Alive();
+            Cell deadCell = Cell.Dead();
+            var neighbors = new List<Cell>();
+            var cell = deadCell.NextGeneration(neighbors);
+            NFluent.Check.That(cell).IsEqualTo(Cell.Dead());
+        }
+
+        [Fact]
+        public void Dead_cell_with_1_neighbor_dies()
+        {
+            Cell deadCell = Cell.Dead();
+            var neighbors = new List<Cell>
+            {
+                Cell.Alive(),
+            };
+            var cell = deadCell.NextGeneration(neighbors);
+            Check.That(cell).IsEqualTo(Cell.Dead());
+        }
+
+        [Fact]
+        public void Dead_cell_with_4_neighbor_still_dead()
+        {
+            Cell deadCell = Cell.Dead();
             var neighbors = new List<Cell>
             {
                 Cell.Alive(),
                 Cell.Alive(),
-                Cell.Dead(),
+                Cell.Alive(),
+                Cell.Alive(),
             };
             var cell = deadCell.NextGeneration(neighbors);
-            Check.That(cell).IsEqualTo(Cell.Alive());
+            Check.That(cell).IsEqualTo(Cell.Dead());
         }
-    }
-
-    internal class Cell
-    {
-        private bool isAlive;
-
-        private Cell(bool isAlive= false)
+      
+        [Fact]
+        public void Dead_cell_with_2_neighbors_still_dead()
         {
-            this.isAlive = isAlive;
-        }
-
-        internal static Cell Alive()
-        {
-            return new Cell(true);
-        }
-
-        internal static Cell Dead()
-        {
-            return new Cell(false);
-        }
-
-        internal bool IsAlive()
-        {
-            return isAlive;
-        }
-
-        internal Cell NextGeneration(List<Cell> neighbors)
-        {
-            if (WithinStabilityThreshold(neighbors))
+            Cell deadCell = Cell.Dead();
+            var neighbors = new List<Cell>
             {
-                return this;
-            }
-
-            if (IsFertilityThreshold(neighbors))
-            {
-                return Cell.Alive();
-            }
-            return Cell.Dead();
+                Cell.Alive(),
+                Cell.Alive(),
+            };
+            var cell = deadCell.NextGeneration(neighbors);
+            Check.That(cell).IsEqualTo(Cell.Dead());
         }
-
-        private static bool IsFertilityThreshold(List<Cell> neighbors)
-        {
-            return neighbors.Count(neighbor => neighbor.isAlive) == 3;
-        }
-
-        private static bool WithinStabilityThreshold(List<Cell> neighbors)
-        {
-            return neighbors.Count(neighbor => neighbor.isAlive) == 2;
-        }
-
-        public override bool Equals(object? obj)
-        {
-            var cell = obj as Cell;
-            if (cell == null)
-                return base.Equals(obj);
-            else
-                return cell.isAlive == this.isAlive;
-        }
+        #endregion
     }
 }
