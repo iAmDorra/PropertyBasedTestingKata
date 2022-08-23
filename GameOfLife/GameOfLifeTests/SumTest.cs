e6a1f1c2aa116cd8efa8d7c4b0a76f45455a4dfb
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2022-08-23 23:15:24 +0200

    Test sum associativity

---
+++ GameOfLife/GameOfLifeTests/SumTest.cs
@@ -0,0 +1,17 @@
+﻿using NFluent;
+
+namespace GameOfLifeTests
+{
+    public class SumTest
+    {
+        [Fact]
+        public void Sum_is_associative()
+        {
+            int a = 1;
+            int b = 2;
+            int c = 3;
+            NFluent.Check.That((a + b) + c)
+                .IsEqualTo(a + (b + c));
+        }
+    }
+}
